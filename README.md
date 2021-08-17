d077f543846dd8585fbc3a37083fd725fd80ce96
edlopez000 <edlopez000>
2021-08-17 13:57:39 -0400

    Finished studio

---
+++ README.md
@@ -0,0 +1,8 @@
+# Fetcha and JSON Studio
+
+[Instructions](https://education.launchcode.org/js-independent-track/chapters/fetch-json/studio.html)
+
+Concepts utilized:
+- fetch()
+- JSON files
+- The esoteric JavaScript sort function
--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// TODO: add code here
+window.addEventListener("load", function () {
+  fetch(
+    "https://handlers.education.launchcode.org/static/astronauts.json"
+  ).then(function (response) {
+    response.json().then(function (json) {
+      let astronauts = "";
+
+      sortedJSON = json.sort(function (a, b) {
+        return b.hoursInSpace - a.hoursInSpace;
+      });
+
+      for (astronaut in sortedJSON) {
+        astronauts += `
+        <div class="astronaut">
+          <h3>${json[astronaut].firstName} ${json[astronaut].lastName}</h3>
+          <ul>
+            <li>Hours in Space: ${json[astronaut].hoursInSpace}</li>
+            <li>Active: ${json[astronaut].active}</li>
+            <li>Skills: ${json[astronaut].skills}</li>
+          </ul>
+          <img src="${json[astronaut].picture}" class="avatar">
+        </div>
+        `;
+      }
+
+      container = document.getElementById("container");
+      container.innerHTML = astronauts;
+    });
+  });
+});
